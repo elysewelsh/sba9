039c175fa281da020ab9e696550606b26c2b2830
elysewelsh <elysecwelsh@gmail.com>
2026-02-24 16:38:43 -0500

    filter dropdowns appear. tasks array added.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import type { DashboardProps, Task }from "../../types"
+import type { DashboardProps, Task, TaskStatus }from "../../types"
 import { TaskForm } from "../TaskForm/TaskForm"
+import { TaskFilter } from "../TaskFilter/TaskFilter"
 
 export function Dashboard ( {text}: DashboardProps) {
 
@@ -22,21 +23,82 @@ export function Dashboard ( {text}: DashboardProps) {
 
 // Implement responsive layout
 // Compose all components into a cohesive dashboard
+
+const ogTasks: Task[] = [
+    {
+        id: "AC",
+        title: "See Alice Cooper Live",
+        description: `"Does this guy know how to party or what?"`,
+        status: 'pending',
+        priority: 'low',
+        dueDate: "November",
+    }
+,
+    {
+        id: "GR",
+        title: "Refuse Gun Rack",
+        description: `"I don't even own ah gun, let alone many guns that would necessitate an entire rack."`,
+        status: "completed",
+        priority: 'low',
+        dueDate: "Yesterday",
+    }
+,
+    {
+        id: "GA",
+        title: "Play Epic Drum Solo",
+        description: `"Thanks. I like to play."`,
+        status: "completed",
+        priority: 'high',
+        dueDate: "Last Month",
+    }
+,
+    {
+        id: "DE",
+        title: "Visit Delaware",
+        description: `"Hi. I'm in...Delaware"`,
+        status: "in-progress",
+        priority: 'low',
+        dueDate: "2027",
+    }
+,
+    {
+        id: "WC",
+        title: "Buy Excalibur",
+        description: `"No Stairway!"`,
+        status: "in-progress",
+        priority:'high',
+        dueDate: "ASAP",
+    }
+,
+    {
+        id: "BR",
+        title: "Road Trip",
+        description: `"If you're gonna spew, spew into this."`,
+        status: "pending",
+        priority: 'medium',
+        dueDate:"Tonight",
+    }
+]
     const [tasks, setTasks] = useState<Task[]>(ogTasks);
     const [filterState, setFilterState] = useState({
         status: "",
         priority: ""
     });
 
-const handleSubmit = (newTask: Task) => {
+    const handleSubmit = (newTask: Task) => {
         //validation util
         setTasks(prevTasks => [...prevTasks, newTask]);
     }
+
+    function handleFilterChange (filters: {status?: TaskStatus, priority?: 'low' | 'medium' | 'high'}) {  
+        setFilterState((prevFilterState) => {return { ...prevFilterState, ...filters };});
+    };
 
     return (
         <>
         <h1>{text}</h1>
         <TaskForm onSubmit={handleSubmit}/>
+        <TaskFilter onFilterChange={handleFilterChange}/>
         </>
     )
 }
